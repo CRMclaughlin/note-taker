d126f8c3b40ddfc5293e6289bc5a732743c1c34c
CRMclaughlin <chrisraymusik@gmail.com>
2022-11-14 17:50:17 -0600

    added function to create new note

--- server.js
+++ server.js
@@ -1,18 +1,26 @@
 import express from 'express';
 import fs from 'fs';
 import path from 'path';
-import db from './db/db.json';
+import db from './db/db.json' assert { type: 'json' };
 import uuid  from 'uuidv4';
 
 const app = express();
-const PORT = process.env.port || 3000;
+const PORT = process.env.port || 3001;
 
 app.use(express.static("public"));
 app.use(express.json());
 
 
+// function to create new note
 
-app.listen(PORT, () => {
-    console.log(`listening on http:localhost:${PORT}`)
-})
+function createNewNote(body, notesArray) {
+    const note = body;
+    notesArray.push(note);
+    fs.writeFileSync(
+        path.join(__dirname, './db.db.json'),
+        JSON.stringify({ notesArray }, null, 2)
+        );
+        return body;
+}
+
 
