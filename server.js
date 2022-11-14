11ccfa438e0d714bcf2f8549d5aa26e8be2a73ce
CRMclaughlin <chrisraymusik@gmail.com>
2022-11-13 18:27:54 -0600

    added routes folder and routes, set up server.js

---
+++ server.js
@@ -0,0 +1,19 @@
+import express from 'express';
+
+import apiRoute from '/routes/apiRoute';
+import htmlRoute from '/rotues/htmlRoute';
+
+const app = express();
+const PORT = 5000;
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+app.use('/', htmlRoute);
+app.use('/api', apiRoute);
+
+app.listen(PORT, () => {
+    console.log(`listening on http:localhost:${PORT}`)
+})
+
